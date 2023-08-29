c99636f4732068572c3f5a2247b3df4c4637ddfe
Jakub Jezynski <kubajezynski@gmail.com>
2023-08-29 16:59:00 +0200

    player climbing stairs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private const float TRANSLATION_SPEED = 5.0f;
 
+    private GameObject lastCollidedStair;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +28,18 @@ public class PlayerMovement : MonoBehaviour
         float targetAngle = MathFunctions.CalculateAngle(Vector2.zero, new Vector2(translationX, translationY));
 
         this.transform.SetPositionAndRotation(position, Quaternion.Euler(0, 0, -targetAngle));
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Stair") && !other.gameObject.Equals(lastCollidedStair))
+        {
+            lastCollidedStair = other.gameObject;
+            this.transform.position = new Vector3(transform.position.x, transform.position.y, other.transform.position.z - other.transform.localScale.z - transform.localScale.z / 2f);
+        }
+        else
+        {
+            lastCollidedStair = null;
+        }
     }
 }
